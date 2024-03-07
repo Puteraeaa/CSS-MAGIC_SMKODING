73438d0c1afef90facfc689d41e1e2f0c783a1f2
putra <utasuta1234@gmail.com>
2024-03-07 23:05:37 +0700

    Tugas 3

---
+++ tugas 3/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "background-tes": "url('./image/bg-testimonial.png')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+};
